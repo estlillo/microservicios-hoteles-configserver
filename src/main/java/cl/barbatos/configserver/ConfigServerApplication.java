b987850328167be7fe483cef3a1c7a41c6e31b4b
Esteban Lillo <estebanlilloc@gmail.com>
2023-08-16 03:42:38 -0400

    Tes cambio en configserver

--- src/main/java/cl/barbatos/configserver/ConfigServerApplication.java
+++ src/main/java/cl/barbatos/configserver/ConfigServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer // Enable Spring Cloud Config Server
+@EnableConfigServer // Enable Spring Cloud Config Server test
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
